4ace0744b0a45787328a54a9dc6f8c54e20aec9b
Zorotool <105043191+Zorotool@users.noreply.github.com>
2022-10-02 13:59:28 +0000

    Deploying to gh-pages from @ Zorotool/goit-react-hw-08-phonebook@c8a274d249312a600216a8f9803b88ebe6fee4bd 🚀

---
+++ static/js/689.0450c702.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[689],{1689:function(e,t,r){r.r(t),r.d(t,{default:function(){return n}});var a="HomePage_title__UFLA3",c=r(184);function n(){return(0,c.jsx)("h1",{className:a,children:"Welcome!"})}}}]);
+//# sourceMappingURL=689.0450c702.chunk.js.map
---
+++ static/js/960.834cba3c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[960],{8960:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("h1",{children:"404 Page not found"})}}}]);
+//# sourceMappingURL=960.834cba3c.chunk.js.map
